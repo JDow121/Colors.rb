7cbf4953aeeeaf0e2a9e39b40562bb5cdf5b8620
JDow121 <50154318+JDow121@users.noreply.github.com>
2019-05-30 18:33:45 -0400

    initial commit

---
+++ colors.rb
@@ -0,0 +1,13 @@
+# Write a program that accepts a color from the user
+puts "enter your fovorite color"
+my_color = gets
+# Compares it with at least three colors you have programmed
+elsif color = blue
+    puts "great job"
+elsif color = orange
+    puts "great job"
+elsif color = red
+    puts "great job"
+end            
+# Prints "Your fovorite color is (your favorite color), nice"
+# Tells you "Never heard of that" if the color is not one of the preprogrammed ones.
