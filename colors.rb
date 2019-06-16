0521c90540aba39f1cae79cf5fbb3a7373417fb5
JDow121 <50154318+JDow121@users.noreply.github.com>
2019-06-16 08:34:06 -0400

    finish colors.rb

--- colors.rb
+++ colors.rb
@@ -1,13 +1,15 @@
 # Write a program that accepts a color from the user
-puts "enter your fovorite color"
-my_color = gets
+puts "enter your favorite color"
+my_color = gets.chomp
 # Compares it with at least three colors you have programmed
-elsif color = blue
-    puts "great job"
-elsif color = orange
-    puts "great job"
-elsif color = red
-    puts "great job"
+if my_color == "blue"
+    puts "Your favorite color is " + my_color + ", nice"
+elsif my_color == "orange"
+    puts "Your favorite color is " + my_color + ", nice"
+elsif my_color == "red"
+    puts "Your favorite color is " + my_color + ", nice"
+else
+    puts "Never heard of that"
 end            
-# Prints "Your fovorite color is (your favorite color), nice"
+# Prints "Your favorite color is (your favorite color), nice"
 # Tells you "Never heard of that" if the color is not one of the preprogrammed ones.
