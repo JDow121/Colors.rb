57e04f4947c54e6b4bd48dc0a8db7f5e7d5cb27a
JDow121 <50154318+JDow121@users.noreply.github.com>
2019-06-16 08:35:42 -0400

    remove comments

--- colors.rb
+++ colors.rb
@@ -1,7 +1,6 @@
-# Write a program that accepts a color from the user
+
 puts "enter your favorite color"
 my_color = gets.chomp
-# Compares it with at least three colors you have programmed
 if my_color == "blue"
     puts "Your favorite color is " + my_color + ", nice"
 elsif my_color == "orange"
@@ -11,5 +10,3 @@ elsif my_color == "red"
 else
     puts "Never heard of that"
 end            
-# Prints "Your favorite color is (your favorite color), nice"
-# Tells you "Never heard of that" if the color is not one of the preprogrammed ones.
